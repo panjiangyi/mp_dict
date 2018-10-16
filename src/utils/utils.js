ee6deb39aaff1c89fe7cf61f55832cf74e4e5608
pjy <panjiangyi@gmail.com>
2018-10-17 00:16:15 +0800

    cloud function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -30,27 +30,17 @@ export default class {
     }
     static getOpenid() {
         return new Promise((res, rej) => {
-            wx.login({
-                success: function (loginCode) {
-                    var appid = 'wxb68e03b957eb1a9c'; //填写微信小程序appid  
-                    var secret = '9110816a35f73a5b716d2ddbfea51fdc'; //填写微信小程序secret  
-
-                    //调用request请求api转换登录凭证  
-                    wx.request({
-                        url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appid + '&secret=' + secret + '&grant_type=authorization_code&js_code=' + loginCode.code,
-                        header: {
-                            'content-type': 'application/json'
-                        },
-                        success(d) {
-                            //获取openid  
-                            res(d.data.openid)
-                        },
-                        fail(d) {
-                            rej(d)
-                        }
-                    })
+            wx.cloud.callFunction({
+                // 云函数名称
+                name: 'getOpenid',
+                // 传给云函数的参数
+                success(res) {
+                    res(res.result.openId);
+                },
+                fail(e) {
+                    rej(e)
                 }
-            })
+            });
         })
 
     }
